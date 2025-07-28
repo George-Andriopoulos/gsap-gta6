67c10507708abfc5e6708bc419bbe42bbc382972
FixCrap <george_andriopoulos@hotmail.gr>
2025-07-28 18:34:17 +0300

    Implement Hero Section

---
+++ src/sections/NavBar.jsx
@@ -0,0 +1,16 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <img
+        src='/images/nav-logo.svg'
+        className='scale-90'
+      />
+      <img
+        src='/images/menu.svg'
+        className='w-10'
+      />
+    </nav>
+  );
+};
+
+export default NavBar;
